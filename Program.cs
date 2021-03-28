76d93afea56609b6201c97bef694131cabfa2745
Maycon-15 <mayconferreira011@gmail.com>
2021-03-28 01:24:21 -0300

    Atividade velocidade média

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Velocidade_media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Double Velocidade, Tempo, Distancia;
+
+            Console.WriteLine("Vamos calculara velocidade media, digite abaixo a distância, em metros (m), que o carro terá de percorrer para chegar ao posto de gasolina");
+            Distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o tempo, em segundos (s), que o carro demorou para chegar ao posto de gasolina:");
+            Tempo = Convert.ToDouble(Console.ReadLine());
+
+            Velocidade = Distancia / Tempo;
+
+            Console.WriteLine($"A velocidade média do carro é de: {Velocidade}");
+        }
+    }
+}
